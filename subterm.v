b467b87001187511afb56f6f039b76e110fd7ce8
B.  L <17168103+liesnikov@users.noreply.github.com>
2019-11-23 17:09:20 +0100

    nats work

--- subterm.v
+++ subterm.v
@@ -3,46 +3,94 @@ Require Import List String.
 Require Import Coq.Program.Equality.
 Import ListNotations MonadNotation.
 
+Definition filteri {A: Type} (f : nat -> A -> bool) (l:list A) : list A :=
+  let go := fix go n l := match l with
+                         | nil => nil
+                         | x :: l => if f n x then x::(go (S n) l) else go (S n) l
+                         end
+  in go 0 l.
 
-Polymorphic Definition subterm_for_ind
-            (npars : nat)
-            (ind : one_inductive_body)
+Definition ind_eq (i0 : inductive) (i1 : inductive) : bool :=
+  andb (String.eqb i0.(inductive_mind) i1.(inductive_mind))
+       (Nat.eqb i0.(inductive_ind) i1.(inductive_ind)).
+
+Definition subterms_for_constructor
+  (refi : inductive) (ref : term) (name: ident) (ct : term) (ncons : nat) (nargs: nat)
+  : list (ident * term * nat)
+  := let nct := subst10 ref ct in
+     let '(ctx, ap) := decompose_prod_assum [] nct in
+     let d :=(map (snd) ∘ filter (fst) ∘
+              mapi (fun i t => (* let '(ctx, ar) := decompose_prod_assum [] (decl_type t) in *)
+                            match (decl_type t) with
+                            | tInd indj _ => if ind_eq indj refi
+                                         then (true, i)
+                                         else (false, 0)
+                            | _ => (false, 0)
+                            end
+              ))
+              ctx in
+     let len := List.length ctx in
+     let construct_cons := fun i =>
+                             it_mkProd_or_LetIn
+                                   ctx
+                                   (tApp (tRel len)
+                                     [tRel (len - (S i));
+                                      tApp (tConstruct refi ncons [])
+                                        (to_extended_list ctx)
+                                     ]
+                                   ) in
+     let renamer := fun i => (name ++ "_subterm_" ++ (string_of_nat i))%string in
+     mapi (fun i '(rel) => (renamer i, construct_cons i, nargs)) d.
+
+Definition subterm_for_ind
+(refi : inductive) (* reference term for the inductive type *)
+(ref : term)
+(npars : nat)
+(pars : context)
+(ind : one_inductive_body)
   : one_inductive_body
   := {| ind_name := (ind.(ind_name) ++ "_direct_subterm")%string ;
-        ind_type  := type_for_direct_subterm npars ind.(ind_type);
+        ind_type  := (tProd nAnon ref (tProd nAnon ref ind.(ind_type))); (* type_for_direct_subterm npars *)
         ind_kelim := [InProp] ;
-        ind_ctors := map (fun '(id, t, k) =>
-                            ((id ++ "_direct_subterm")%string,
-                             (tRel 0), k))
-                         ind.(ind_ctors);
+        ind_ctors :=
+          List.concat (mapi (fun n '(id, t, k) =>
+                               subterms_for_constructor refi ref id t n k)
+                           ind.(ind_ctors));
         ind_projs := [] |}.
 
-Polymorphic Definition subterm_for_mutual_ind
+Definition subterm_for_mutual_ind
             (mind : mutual_inductive_body)
-            (ind0 : inductive)
+            (ind0 : inductive) (* internal metacoq representation of inductive, part of tInd *)
+            (ref  : term) (* reference term for the inductive type, like (tInd {| inductive_mind := "Coq.Init.Datatypes.nat"; inductive_ind := 0 |} []) *)
                   : mutual_inductive_body
   := let i0 := inductive_ind ind0 in
      {| ind_finite := BasicAst.Finite;
-        ind_npars := mind.(ind_npars);
-        ind_universes := mind.(ind_universes) ;
-        ind_params := mind(.ind_params);
-        ind_bodies := ((map (subterm_for_ind mind.(ind_npars)) ∘
+        ind_npars := 0;
+        ind_universes := mind.(ind_universes);
+        ind_params := [];
+        ind_bodies := (map (subterm_for_ind ind0 ref mind.(ind_npars) mind.(ind_params)) ∘
                       (filteri (fun (i : nat) (ind : one_inductive_body) =>
                                   if Nat.eqb i i0 then true else false)))
                       mind.(ind_bodies);
         |}.
 
+Inductive nnat : Type :=
+  no : nnat
+| none : nnat -> nnat
+| ntwo : nnat -> nnat -> nnat.
 
 Polymorphic Definition subterm (tm : Ast.term)
   : TemplateMonad unit
   := match tm with
      | tInd ind0 _ =>
-       decl <- tmQuoteInductive (inductive_mind ind0) ;;
-       let subt := subterm_for_mutual_ind decl ind0 in
-       tmMkInductive' subt
+       decl <- tmQuoteInductive (inductive_mind ind0);;
+       let subt := subterm_for_mutual_ind decl ind0 tm in
+       v <- tmEval cbv subt;;
+       tmPrint v;;
+       tmMkInductive' v
      | _ => tmPrint tm;; tmFail "is not an inductive"
      end.
 
-Run TemplateProgram (subterm <%list%>).
+Run TemplateProgram (subterm <%nnat%>).
 
-Print nat_direct_subterm.
+Print nnat_direct_subterm.
